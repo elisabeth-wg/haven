2f077433c4e182efa2aa03d9279a2aee6be8c00a
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-08-10 20:39:40 -0500

    Add tailwind and removing bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.ejs',
+    './server.js',
+    './config/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
