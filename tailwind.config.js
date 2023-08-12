39b38fda8e0c62ce4fd2e616bd515332a1b6579e
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-08-11 20:30:09 -0500

    Work on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './views/partials/**/*.ejs',
     './views/**/*.ejs',
     './server.js',
     './config/**/*.js',
+    './controllers/**/*.js',
+    './middleware/**/*.js',
+    './models/**/*.js',
+    './routes/**/*.js',
   ],
   theme: {
     extend: {},
